6dd3721fd212bc201f729a69dfd1b8d7a4ba68b6
Manivasagam <manivasaga.mca16@gmail.com>
2020-03-28 22:18:32 +0530

    Push First Time

---
+++ src/test/java/com/manivash/security/springbootfacebookloginexample/SpringBootFacebookLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manivash.security.springbootfacebookloginexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFacebookLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
